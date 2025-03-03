64bdd2ba43651f5fdc80dec09120fb3956f92f97
Jesus <jeburga9708@gmail.com>
2025-03-03 11:59:58 -0500

    Conection with DB done

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import userRoutes from './routes/userRoutes.js';
-import express from 'express';
+import userRoutes from "./routes/userRoutes.js";
+import express from "express";
+import db from "./config/db.js";
 
 const app = express();
 
+// Conexión a la BD
+try {
+  await db.authenticate();
+  console.log("Conexión establecida correctamente con MySQL.");
+} catch (error) {
+  console.error("No se pudo conectar a la base de datos:", error);
+}
+
 // Habilitar Pug
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 // Carpeta Pública
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 // Routing
-app.use('/auth', userRoutes);
+app.use("/auth", userRoutes);
 
 // Definir puerto
-app.listen(3000)
+app.listen(3000);
